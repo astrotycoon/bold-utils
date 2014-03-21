1204b2196d0e70be7e84895ac7aa531ec9fcac10
Luba Tang <lubatang@gmail.com>
2014-03-21 20:37:13 +0800

    Add StringList's unit-test

---
+++ tools/unittests/StringListTest.cpp
@@ -0,0 +1,31 @@
+//===- StringListTest.cpp --------------------------------------------------===//
+//
+//                             The Bold Project
+//
+//  This file is distributed under the New BSD License.
+//  See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+#include <bold/ADT/StringList.h>
+#include <pat/pat.h>
+
+using namespace bold;
+
+//===----------------------------------------------------------------------===//
+// Testcases
+//===----------------------------------------------------------------------===//
+PAT_F(StringListCase, size_test)
+{
+  StringRef a("a");
+  StringRef b("b");
+  StringRef c("c");
+
+  StringList list;
+  list.append(a);
+  list.append(b);
+  list.append(c);
+
+  ASSERT_TRUE(3 == list.size());
+  ASSERT_FALSE(list.empty());
+}
+
